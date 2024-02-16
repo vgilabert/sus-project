a520fc525d2ca48fa6c7e8a5ef3b8e9af2df9078
vgilabert <vincent.gilabert@gmail.com>
2024-02-16 20:45:21 +0100

    Fix some errors in turrets behaviors

--- Assets/Scripts/Train/Missile.cs
+++ Assets/Scripts/Train/Missile.cs
@@ -5,30 +5,43 @@ namespace Train
 {
     public class Missile : Wagon
     {
+        [SerializeField] private float explosionRadius;
+        
         [SerializeField] private float rangeMin;
         [SerializeField] private float rangeMax;
 
-        [Header("Debug"), Space(5)]
-        [SerializeField] protected bool showRangeGizmos;
-
         protected override IEnumerator Shoot()
         {
-            while (Target)
+            CanShoot = false;
+            var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (var enemy in enemies)
             {
-                if (IsFacingTarget)
+                var distance = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distance <= explosionRadius)
                 {
-                    var enemies = GameObject.FindGameObjectsWithTag("Enemy");
-                    foreach (var enemy in enemies)
-                    {
-                        var distance = Vector3.Distance(transform.position, enemy.transform.position);
-                        if (distance >= rangeMin && distance <= rangeMax)
-                        {
-                            enemy.GetComponent<Enemy>().TakeHit(damage, new RaycastHit());
-                        }
-                    }
+                    StartCoroutine(ShowExplosion(enemy.transform.position));
+                    enemy.GetComponent<Enemy>().TakeHit(damage, new RaycastHit());
                 }
-                yield return new WaitForSeconds(timeBetweenShots);
             }
+
+            yield return new WaitForSeconds(timeBetweenShots);
+            CanShoot = true;
+        }
+        
+        #region Debug
+
+        [Header("Debug"), Space(5)]
+        [SerializeField] protected bool showRangeGizmos;
+        
+        private bool showExplosion;
+        private Vector3 explosionPosition;
+        
+        IEnumerator ShowExplosion(Vector3 position)
+        {
+            showExplosion = true;
+            explosionPosition = position;
+            yield return new WaitForSeconds(timeBetweenShots);
+            showExplosion = false;
         }
         
         private void OnDrawGizmos()
@@ -37,6 +50,15 @@ namespace Train
             Gizmos.color = Color.red;
             Gizmos.DrawWireSphere(transform.position, rangeMin);
             Gizmos.DrawWireSphere(transform.position, rangeMax);
+            
+            if (showExplosion)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(explosionPosition, explosionRadius);
+            }
         }
+            
+        #endregion
+        
     }
 }
