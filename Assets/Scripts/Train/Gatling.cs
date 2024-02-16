edd03fbaf8fdb78997a3c84bb9fcf9dd0be34f66
vgilabert <vincent.gilabert@gmail.com>
2024-02-17 00:37:29 +0100

    Add line trace effect on gatling

--- Assets/Scripts/Train/Gatling.cs
+++ Assets/Scripts/Train/Gatling.cs
@@ -9,9 +9,14 @@ namespace Train
     {
         [SerializeField] private float range;
 
+        [SerializeField] private GameObject BulletTrailEffect;
+        
         protected override IEnumerator Shoot()
         {
             CanShoot = false;
+            var trail = Instantiate(BulletTrailEffect, transform.GetChild(0).position, transform.GetChild(0).rotation);
+            var trailScript = trail.GetComponent<LineTrace>();
+            trailScript.SetTargetPosition(Target.transform.position);
             Target.TakeHit(ActualDamage, new RaycastHit());
             
             yield return new WaitForSeconds(TimeBetweenShots);
